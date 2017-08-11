a495ebbdc70c0566b82cbbba4ee6b9439dc699f3
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-08-11 17:31:18 +0500

    kubernetes deployment

--- kubernetes/images/generator/setup.sh
+++ kubernetes/images/generator/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-git clone ${REPOSITORY} susi_viberbot
-cd susi_viberbot
+git clone ${REPOSITORY} susi_fbbot
+cd susi_fbbot
 git checkout ${BRANCH}
 
 if [ -v COMMIT_HASH ]; then
     git reset --hard ${COMMIT_HASH}
 fi
 
+rm -rf .git
 npm install --no-shrinkwrap
